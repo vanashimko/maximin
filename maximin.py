652fde195a650f00be2186f7bb1a2f6636f5cbf1
vanashimko <vana.shimko@gmail.com>
2017-02-14 13:34:43 +0300

    Refactor

--- maximin.py
+++ maximin.py
@@ -4,14 +4,10 @@ from collections import defaultdict
 from statistics import mean
 
 
-def sqr(a):
-    return a*a
-
-
 def distance(a, b):
     if len(a) != len(b):
         raise ValueError('Vectors have different dimensions')
-    return sqrt(sum(map(sqr, (x-y for x, y in zip(a, b)))))
+    return sqrt(sum(((x-y)**2 for x, y in zip(a, b))))
 
 
 def allocate_clusters(vectors, centroids):
@@ -23,7 +19,8 @@ def allocate_clusters(vectors, centroids):
 
 
 def average_centroids_distance(clusters):
-    return mean((distance(x, y) for x in clusters.keys() for y in clusters.keys()))
+    centroids = list(clusters.keys())
+    return mean((distance(x, y) for i, x in enumerate(centroids) for y in centroids[i:]))
 
 
 def get_new_centroid_index(vectors, clusters):
